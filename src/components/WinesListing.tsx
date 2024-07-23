e3227fd89ba9eddf0a2650bf7127a1138c18cf3f
Deni <denitrajkov@yahoo.com>
2024-07-23 08:41:00 +0200

    example

--- src/components/WinesListing.tsx
+++ src/components/WinesListing.tsx
@@ -1,43 +1,11 @@
-import Card from "@/components/Card";
-import { fetchData } from "@/fetchData";
-import { useEffect, useState } from "react";
 import { WineProps } from "@/interface/type";
-import FilterButton from "./FilterButton";
+import Card from "@/components/Card";
 
-export default function WinesListing() {
-  const [wines, setWines] = useState<WineProps[]>([]);
-  const [filteredWines, setFilteredWines] = useState<WineProps[]>([]);
+interface WinesListingProps {
+  wines: WineProps[];
+}
 
-  useEffect(() => {
-    const fetchDataAndSetWines = async () => {
-      try {
-        const endpoint = "wines";
-        const data = await fetchData(endpoint);
-        setWines(data);
-        setFilteredWines(data);
-      } catch (error) {
-        console.error("Error fetching wine data:", error);
-      }
-    };
-
-    fetchDataAndSetWines();
-  }, []);
-
-  const filterWines = (items: WineProps[], criteria: string) => {
-    return items.filter((item) => {
-      return item.color === criteria;
-    });
-  };
-
-  const handleFilterClick = (criteria: string) => {
-    if (criteria === "") {
-      setFilteredWines(wines);
-    } else {
-      const filtered = filterWines(wines, criteria);
-      setFilteredWines(filtered);
-    }
-  };
-
+export default function WinesListing({ wines }: WinesListingProps) {
   const addToCart = (wine: WineProps) => {
     const existingItems = JSON.parse(localStorage.getItem("cartItems") ?? "[]");
 
@@ -47,39 +15,19 @@ export default function WinesListing() {
   };
 
   return (
-    <>
-      <div className="text-center p-5 text-wine bg-cream mb-5 flex flex-wrap justify-center">
-        <FilterButton
-          filterType="Бело"
-          label="Бели вина"
-          onClick={handleFilterClick}
-        />
-        <FilterButton
-          filterType="Црвено"
-          label=" Црвени вина"
-          onClick={handleFilterClick}
-        />
-        <FilterButton
-          filterType=""
-          label="Сите вина"
-          onClick={handleFilterClick}
-        />
+    <div>
+      <div className="flex flex-wrap justify-center justify-around text-center">
+        {wines.map((wine: WineProps) => (
+          <Card
+            key={wine.id}
+            image={wine.image}
+            name={wine.name}
+            id={wine.id}
+            color={wine.color}
+            addCardToShop={() => addToCart(wine)}
+          />
+        ))}
       </div>
-
-      <div>
-        <div className="flex flex-wrap justify-center justify-around text-center">
-          {filteredWines.map((wine: WineProps) => (
-            <Card
-              key={wine.id}
-              image={wine.image}
-              name={wine.name}
-              id={wine.id}
-              color={wine.color}
-              addCardToShop={() => addToCart(wine)}
-            />
-          ))}
-        </div>
-      </div>
-    </>
+    </div>
   );
 }
