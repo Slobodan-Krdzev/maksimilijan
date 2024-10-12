30c6d269734d3720fa935fe8eaa0da7f82c599a9
denitrajkov <denitrajkov@yahoo.com>
2024-10-12 14:42:07 +0200

    change image

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,6 +3,7 @@ import Banner from "@/components/Banner";
 import WinesListing from "@/components/WinesListing";
 import { generateMetadata } from "@/metadata/generateMetadata";
 import { Metadata } from "next";
+import React from "react";
 
 export default function Products() {
   return (
