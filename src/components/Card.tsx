7336213633aa1ebc5f9e3936a609fb8b9cf934ad
denitrajkov <denitrajkov@yahoo.com>
2024-10-20 13:09:45 +0200

    add sceleton

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,31 +1,59 @@
+"use client";
+
+import { useState } from "react";
 import { WineProps } from "@/interface/type";
 import Image from "next/image";
 import Link from "next/link";
 
 function Card({ id, name, color, image }: WineProps) {
+  const [isLoading, setIsLoading] = useState(true); // Лоадинг состојба
+
   return (
     <div className="relative text-wine bg-white shadow-md bg-clip-border rounded-lg w-80 mb-12 transform transition duration-500 hover:scale-110">
+      {/* Image Skeleton */}
       <div className="relative w-full h-64 rounded-t-lg overflow-hidden">
+        {isLoading && (
+          <div className="w-full h-full animate-pulse">
+            <div className="h-full bg-gray-300 rounded-t-lg"></div>
+          </div>
+        )}
         <Image
           src={image}
           alt="card-image"
           fill
-          className="object-contain"
+          className={`object-contain transition-opacity duration-500 ${
+            isLoading ? "opacity-0" : "opacity-100"
+          }`}
           loading="lazy"
+          onLoadingComplete={() => setIsLoading(false)}
         />
       </div>
+
+      {/* Text Skeleton */}
       <div className="p-6">
-        <div className="text-center">
-          <p className="text-2xl">{name}</p>
-        </div>
+        {isLoading ? (
+          <div className="animate-pulse space-y-3">
+            <div className="h-6 bg-gray-300 rounded w-3/4"></div>
+            <div className="h-4 bg-gray-200 rounded w-1/2"></div>
+          </div>
+        ) : (
+          <div className="text-center">
+            <p className="text-2xl">{name}</p>
+          </div>
+        )}
       </div>
 
+      {/* Button Skeleton */}
       <div className="py-3 flex">
-        <Link href={`/products/${id}`} className="w-full max-w-96">
-          <button className="border px-10 py-3 border-wine text-xl hover:bg-wine hover:text-white transition duration-300 ease-in-out">
-            детали
-          </button>
-        </Link>
+        {isLoading ? (
+          <div className="w-full h-12 bg-gray-300 rounded animate-pulse"></div>
+        ) : (
+          <Link href={`/products/${id}`} className="w-full max-w-96">
+            <button className="border px-10 py-3 border-wine text-xl hover:bg-wine hover:text-white transition duration-300 ease-in-out">
+              детали
+            </button>
+          </Link>
+        )}
       </div>
     </div>
   );
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,16 @@
 "use client";
-import React, { useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import React, { Fragment, useState } from "react";
 
 export default function Form() {
+  const [modalMessage, setModalMessage] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     phone: "",
-    message: "",
+    numberPeople: "",
   });
 
   const handleChange = (e: any) => {
@@ -15,6 +19,51 @@ export default function Form() {
       [e.target.id]: e.target.value,
     });
   };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const data = {
+      ...formData,
+      to_email: "denitrajkov@yahoo.com",
+      to_name: "Ивица",
+      from_firstName: formData.name,
+      from_lastName: formData.email,
+      from_number: formData.phone,
+      from_town: formData.numberPeople,
+    };
+
+    try {
+      const response = await fetch("https://formspree.io/f/xanwnvrv", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        setModalMessage("Вашата порака е испратена!");
+        openModal();
+      } else {
+        setModalMessage("Грешка при испраќање на пораката.");
+        openModal();
+      }
+    } catch (error) {
+      setModalMessage("Грешка при испраќање на пораката.");
+      openModal();
+    }
+
+    setFormData({
+      name: "",
+      email: "",
+      phone: "",
+      numberPeople: "",
+    });
+  };
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
 
   return (
     <div className="p-5">
@@ -27,7 +76,7 @@ export default function Form() {
             Пополнете ги податоците за Вас и Вашата нарачка. Нашиот тим ќе ве
             исконтактира веднаш со цел да се потврди нарачката. Taste the Wine!
           </p>
-          <form className="space-y-8">
+          <form className="space-y-8" onSubmit={handleSubmit}>
             <div>
               <label className="block mb-2 text-sm font-medium text-wine">
                 Вашиот email
@@ -74,10 +123,10 @@ export default function Form() {
               </label>
               <input
                 type="number"
-                id="phone"
+                id="numberPeople"
                 className="shadow-sm text-wine border border-wine text-sm rounded-lg focus:ring-wine1 block w-full p-2.5 dark:placeholder-wine1 dark:text-wine dark:focus:border-wine1"
                 placeholder="4"
-                value={formData.message}
+                value={formData.numberPeople}
                 onChange={handleChange}
               />
             </div>
@@ -89,6 +138,57 @@ export default function Form() {
             </button>
           </form>
         </div>
+        <Transition appear show={isModalOpen} as={Fragment}>
+          <Dialog as="div" className="relative z-10" onClose={closeModal}>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-black bg-opacity-25" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 overflow-y-auto">
+              <div className="flex min-h-full items-center justify-center p-4 text-center">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 scale-95"
+                  enterTo="opacity-100 scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 scale-100"
+                  leaveTo="opacity-0 scale-95"
+                >
+                  <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg font-medium leading-6 text-gray-900"
+                    >
+                      Известување
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">{modalMessage}</p>
+                    </div>
+
+                    <div className="mt-4">
+                      <button
+                        type="button"
+                        className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                        onClick={closeModal}
+                      >
+                        Затвори
+                      </button>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </Dialog>
+        </Transition>
       </div>
     </div>
   );
