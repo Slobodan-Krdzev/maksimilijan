cccd14199c26ffdbcfc95f6a5e9b004a333f659d
denitrajkov <denitrajkov@yahoo.com>
2024-11-10 09:19:56 +0100

    change image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,10 @@ import { Citat } from "@/components/Citat";
 export default function Home() {
   return (
     <>
-      <Banner imageSrc="/winery.jpg" text="Maksimilijan Family Winery!" />
+      <Banner
+        imageSrc="/accomodation.webp"
+        text="Maksimilijan Family Winery!"
+      />
       <AboutSection image="/outdoor.jpg" />
       <Citat />
       <AboutSection reverse={true} image="/bottles.jpg" />
